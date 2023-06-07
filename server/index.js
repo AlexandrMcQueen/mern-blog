e9d784132e3e304f0f84070d376503db21796a6d
SAnches <oleksandrmoculskij@gmail.com>
2023-06-07 11:44:06 +0300

    cors remove changes

--- server/index.js
+++ server/index.js
@@ -27,10 +27,7 @@ app.use(cors({
 
 }))
 
-app.use(function (req, res, next){
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "*")
-})
+
 
 app.use('/uploads',express.static(path.join(__dirname,'/uploads')));
 
