2e8767ce26ed5d1fad5234636b567ee912c123b4
SAnches <oleksandrmoculskij@gmail.com>
2023-06-16 00:06:19 +0300

    Five hours later...

--- server/index.js
+++ server/index.js
@@ -32,8 +32,13 @@
     }))
 
 
-    app.use('/',authRoutes);
-    app.use('/',postRoutes)
+    app.use(express.static(path.join(__dirname, 'build')));
+
+    // Serve the index.html file as a fallback for all other routes
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+    });
+
 
 
 
