ae3bee955172bfcaa826c434936cfbd92e0411b7
SAnches <oleksandrmoculskij@gmail.com>
2023-06-16 00:25:58 +0300

    ff

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -66,7 +66,9 @@ export const login = async (req,res) => {
 
             res.cookie('token',token,{
                 sameSite:"none",
-                httpOnly:true
+                expiresIn: '1d',
+                httpOnly:true,
+                secure:true
             }).json('ok');
         })
 
